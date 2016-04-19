8756bd6b9dd990be4269eb15b55f2a434594568a
litianzhao <tianzhao.li@bitmain.com>
2016-04-19 17:27:55 +0800

    移除多余文件；修复容器注册错误

--- src/FisServiceProvider.php
+++ src/FisServiceProvider.php
@@ -6,7 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class FisServiceProvider extends ServiceProvider {
     public function register() {
+        $this->mergeConfigFrom(__DIR__ . '/config/fis.php', 'fis');
 
+        $this->app->singleton(Fis::class, function() {
+            $path = public_path('assets/assets.json');
+            return new Fis($path);
+        });
+
+        $this->app->alias(Fis::class, 'fis');
     }
 
     public function boot() {
